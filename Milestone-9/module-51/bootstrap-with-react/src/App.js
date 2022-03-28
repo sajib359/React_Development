7204297a01e1a85ffa4ffdb00d86f97270349bd2
Sajib Hossain <sajib.sj359@gmail.com>
2022-03-28 14:24:01 +0600

    added bootstrap

--- Milestone-9/module-51/bootstrap-with-react/src/App.js
+++ Milestone-9/module-51/bootstrap-with-react/src/App.js
@@ -1,18 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <loadCountry></loadCountry>
+     <cardGroup></cardGroup>
+      
     </div>
   );
-}
-
-function loadCountry(){
-  <div>
-    <h1>Hellow World i am Comming to Visiting !!! </h1>
-  </div>
 }
 
 export default App;
---
+++ Milestone-9/module-51/bootstrap-with-react/src/Components/cardGroup/cardGroup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const cardGroup = () => {
+    return (
+        <div>
+            <h2>Joy Bangla Jitbe abar Nouka</h2>
+        </div>
+    );
+};
+
+export default cardGroup;
