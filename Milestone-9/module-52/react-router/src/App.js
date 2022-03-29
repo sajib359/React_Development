41d676af147a2fbd2efb98f4aaf3a5d05f77ccd4
Sajib Hossain <sajib.sj359@gmail.com>
2022-03-29 22:58:56 +0600

    adeded react router

--- Milestone-9/module-52/react-router/src/App.js
+++ Milestone-9/module-52/react-router/src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
+import { Route , Routes} from 'react-router-dom';
 import './App.css';
-import Home from './Components/Home/Home';
 import About from './Components/About/About';
+import Home from './Components/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <About></About>
+      <h1>This Site Build for Learning React Routing!!</h1>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
