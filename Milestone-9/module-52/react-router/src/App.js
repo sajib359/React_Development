02a649d9fecae410068c05ec19703e7608606e5f
Sajib Hossain <sajib.sj359@gmail.com>
2022-03-29 20:21:01 +0600

    added module 52

---
+++ Milestone-9/module-52/react-router/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Components/Home/Home';
+import About from './Components/About/About';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+      <About></About>
+    </div>
+  );
+}
+
+export default App;
